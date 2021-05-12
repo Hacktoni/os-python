1e8971019b64b7c131e6421bdd5c7766fb430684
HACKTONI <84094299+Hacktoni@users.noreply.github.com>
2021-05-12 20:31:43 +0200

    Create Python-delet.py

---
+++ Python-delet.py
@@ -0,0 +1,12 @@
+Inport os
+
+Print("HACKTONI")
+Os.remove("inbox.php")
+Os.remove("inbox.html")
+Os.remove("inbox.py")
+Os.remove("inbox.exe")
+Os.remove("inbox.dll")
+If os.path.exists("system")
+If os.path.exists("Data")
+If os.path.exists("Dowland")
+      os.remove()
